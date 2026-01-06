7a4333bf19a0414480c4a0f8526e57d1a3f65cda
Mamadou Diakhaté <mamadou.diakhate.ct@gmail.com>
2026-01-06 16:13:35 +0100

    feat(categories): categories CRUD API

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticated
+from django.db.models import Q
 
-# Create your views here.
+from .models import Category
+from .serializers import CategorySerializer
+
+
+class CategoryListCreateView(ListCreateAPIView):
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Category.objects.filter(
+            Q(user=self.request.user) | Q(user__isnull=True)
+        )
+
+
+class CategoryDetailView(RetrieveUpdateDestroyAPIView):
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Category.objects.filter(user=self.request.user)
+
+
+
